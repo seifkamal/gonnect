38718c4ae4c4d8711fa0562f4b0c999ae3665ec9
Seif Kamal <seif@tickx.co.uk>
2020-02-27 20:54:08 +0000

    Add schema.sql

---
+++ schema.sql
@@ -0,0 +1,26 @@
+USE `gonnect`;
+
+CREATE TABLE IF NOT EXISTS `player`
+(
+    `id`    INT                        NOT NULL AUTO_INCREMENT,
+    `alias` VARCHAR(64) UNIQUE         NOT NULL,
+    `state` ENUM ('online', 'offline') NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `match`
+(
+    `id`    INT                        NOT NULL AUTO_INCREMENT,
+    `state` ENUM ('creating', 'ready') NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `match_players`
+(
+    `id`        INT NOT NULL AUTO_INCREMENT,
+    `match_id`  int,
+    `player_id` int,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`match_id`) REFERENCES `match` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`player_id`) REFERENCES `player` (`id`) ON DELETE CASCADE
+);
