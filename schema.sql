6acd25a757031b6694bef97d0584948036a4e610
Seif Kamal <seif@tickx.co.uk>
2020-03-08 00:57:11 +0000

    Add default timestamp values

--- schema.sql
+++ schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS `players`
     `id`         INT                               NOT NULL AUTO_INCREMENT,
     `alias`      VARCHAR(64) UNIQUE                NOT NULL,
     `state`      ENUM ('searching', 'unavailable') NOT NULL,
-    `created_at` DATETIME                          NOT NULL,
-    `updated_at` DATETIME                          NOT NULL,
+    `created_at` DATETIME                          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME                          NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 );
 
@@ -17,8 +17,8 @@ CREATE TABLE IF NOT EXISTS `matches`
 (
     `id`         INT                     NOT NULL AUTO_INCREMENT,
     `state`      ENUM ('ready', 'ended') NOT NULL,
-    `created_at` DATETIME                NOT NULL,
-    `updated_at` DATETIME                NOT NULL,
+    `created_at` DATETIME                NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME                NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 );
 
@@ -27,8 +27,8 @@ CREATE TABLE IF NOT EXISTS `matches_players`
     `id`         INT      NOT NULL AUTO_INCREMENT,
     `match_id`   int,
     `player_id`  int,
-    `created_at` DATETIME NOT NULL,
-    `updated_at` DATETIME NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`match_id`) REFERENCES `matches` (`id`) ON DELETE CASCADE,
     FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE
